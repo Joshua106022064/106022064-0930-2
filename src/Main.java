cdb7c1dc7f8fb971a699e25b0ffdb104c00cede4
unknown <joshua31041@gmail.com>
2017-09-30 14:34:02 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int x;
+        x=in.nextInt();
+        System.out.printf("%.1f",x*1.6);
+        System.out.println();
+
+    }
+}
